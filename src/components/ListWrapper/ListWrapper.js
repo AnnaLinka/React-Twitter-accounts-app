1b99643e8d54f9f25b3f87c6c944f7ebec9e384d
Anna Linka <annazlinka@gmail.com>
2019-05-15 13:00:01 +0200

    Split into components, add scss, refactor

--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -1,43 +1,13 @@
-import React from 'react';
-import ListItem from './ListItem/ListItem';
-import './ListWrapper.css';
-import { twitterAccounts } from '../../data/twitterAccounts';
+import React from "react";
+import ListItem from "./ListItem/ListItem";
+import styles from "./ListWrapper.module.scss";
 
-// przed destrukturyzacja:
-// const ListWrapper = () => (
-//     <ul className="listWrapper__wrapper">
-//         {twitterAccounts.map(item=> (
-//              <ListItem
-//              name={item.name}
-//              description={item.description}
-//              image={item.image}
-//              link={item.link}
-//          />
-//         ))}
-//     </ul>
-// );
-
-//po destrukturyzacji pierwszej
-// const ListWrapper = () => (
-//     <ul className="listWrapper__wrapper">
-//         {twitterAccounts.map(({name, description, image, twitterLink}) => (
-//                 <ListItem
-//                 name={name}
-//                 description={description}
-//                 image={image}
-//                 link={twitterLink}
-//             />
-//         ))}
-//     </ul>
-// );
-
-//po destrukturyzacji 2 ze spread operator 
-const ListWrapper = () => (
-    <ul className="listWrapper__wrapper">
-        {twitterAccounts.map( item => (
-                <ListItem key={item.name} {...item}
-            />
-        ))}
-    </ul>
+const ListWrapper = props => (
+  <ul className={styles.wrapper}>
+    {props.items.map(item => (
+      <ListItem key={item.name} {...item} />
+    ))}
+  </ul>
 );
+
 export default ListWrapper;
